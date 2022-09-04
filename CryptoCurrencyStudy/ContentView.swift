128337c4d18344fa71c93b7c475448fa46bacea3
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-09-04 12:31:38 +0300

    Initial Commit

---
+++ CryptoCurrencyStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCurrencyStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 4.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoCurrencyStudy/CryptoCurrencyStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyStudyApp.swift
+//  CryptoCurrencyStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 4.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
